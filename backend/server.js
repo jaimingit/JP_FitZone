3b012107a53a466eb430ea4dc7ac5c79cef043c4
jaimin patel <jaiminpatel@192.168.1.3>
2025-09-24 16:14:42 +0530

    Update backend API URLs to Render frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['https://jp-fit-zone.vercel.app/'],
+    credentials: true,
+  }
+));
 app.use(express.json());
 
 // Routes
